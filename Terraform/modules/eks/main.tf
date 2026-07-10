655d5d1b4c44a9663da7a161d0a498ef4b5b5b2d
vkharishb <v.harish.bodapati@gmail.com>
2026-07-10 12:05:59 +0530

    format check issues resolved

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -54,7 +54,7 @@ resource "aws_eks_cluster" "this" {
 
   access_config {
     authentication_mode = "API_AND_CONFIG_MAP"
-     bootstrap_cluster_creator_admin_permissions = true
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   # Encrypt Kubernetes secrets at rest, on top of EBS/etcd volume encryption.
