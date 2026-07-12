65b509dfa7c072d87ef2c94bc373dd1a46c9a8d3
vkharishb <v.harish.bodapati@gmail.com>
2026-07-12 11:08:44 +0530

    added Storageclass and install CSI drivers into cluster

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -6,9 +6,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
     }
   }
 
